01147ffb9e8b87dd2c0b8e061a021b3ebe4b1484
chromium7 <christofer@ui.co.id>
2024-08-18 16:50:16 +0700

    Activity no photos

--- tracker/web/activities/urls.py
+++ tracker/web/activities/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('list/', views.strava_list, name="strava_list"),
     path('<int:id>/', views.details, name="details"),
+    path('<int:id>/mark-no-photos/', views.mark_no_photos, name="mark_no_photos"),
     path('<int:id>/photos/add/', views.add_photo, name="add_photo"),
     path('<int:id>/photos/<int:photo_id>/edit/', views.edit_photo, name="edit_photo"),
 ]
